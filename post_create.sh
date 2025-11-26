8c49eb0632e14271822ded7a47f93d3d74361bc4
Matthew Ngan <58377002+cwngan@users.noreply.github.com>
2025-11-26 23:20:33 +0800

    add network utils for simulations

--- post_create.sh
+++ post_create.sh
@@ -2,5 +2,5 @@
 
 cd /workspaces/CSCI4430-project
 sudo apt update && sudo apt -y install clangd
-sudo apt install -y pulseaudio opus-tools libopus-dev alsa-utils
+sudo apt install -y pulseaudio opus-tools libopus-dev alsa-utils iputils-ping
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .
